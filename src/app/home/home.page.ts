f6abc5d4f06e2d2d32f8596d9d5a0c16e7245dcd
Vishal Darekar <vish.darekar@gmail.com>
2024-07-10 14:33:51 +0530

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import * as L from 'leaflet';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,6 +8,37 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   standalone: true,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+
+  private map: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map').fitWorld()
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap'
+  }).addTo(this.map);
+
+    this.map.locate({ setView: true, maxZoom: 16 });
+
+    this.map.on('locationfound', (e: any) => {
+      var radius = e.accuracy;
+
+      L.marker(e.latlng).addTo(this.map)
+          .bindPopup("You are within " + radius + " meters from this point").openPopup();
+  
+      L.circle(e.latlng, radius).addTo(this.map);
+    });
+
+    this.map.on('locationerror', (e: any) => {
+      alert(e.message);
+    });
+  }
 }
